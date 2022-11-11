19720b23579ea4e194e8730fc72512a7b4f079d4
Vera Lobova <pchilli@yandex.ru>
2022-11-11 12:21:09 +0300

    all but one

---
+++ TASK3/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 58: Задайте две матрицы. 
+//Напишите программу, которая будет находить произведение двух матриц.
+//(посмотрите как реализуется произведение матриц, там не просто перемножение элемент на элемент)
+/*Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+void Main()
+{
+    Console.Clear();
+    int[,] firstArray = new int[2, 2];
+    FillArray(firstArray);
+
+    int[,] secondArray = new int[2, 2];
+    FillArray(secondArray);
+Console.WriteLine("first arr ");
+    PrintArray(firstArray);
+    Console.WriteLine("*");
+Console.WriteLine("sec arr ");
+    PrintArray(secondArray);
+    Console.WriteLine("=");
+
+ MultiplyArrays(firstArray, secondArray);
+   
+
+
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 5);
+        }
+
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]}");
+        }
+        Console.WriteLine();
+
+    }
+
+    Console.WriteLine();
+}
+
+void MultiplyArrays(int[,] arr1, int[,] arr2)
+{
+    int[,] resultArr = new int[arr1.GetLength(0), arr2.GetLength(1)]; 
+
+    if (arr1.GetLength(1) == arr2.GetLength(0))
+    {
+        for (int i = 0; i <arr1.GetLength(0) ; i++)//1
+
+        {
+            for(int j = 0; j<arr2.GetLength(1); j++)//3
+            {
+                
+                for (int k = 0; k < arr1.GetLength(1); k++)//1!
+                {
+                    
+                
+                resultArr[i,j] += (arr1[i,k]*arr2[k,j]);
+                
+                }
+            }         
+            
+            
+        }
+          PrintArray(resultArr);
+
+     
+
+    }
+    else
+    {
+        Console.WriteLine("Умножить две матрицы можно только в том случае, если число столбцов первой, равняется числу строк второй. \nИзмените размер матрицы и попробуйте снова");
+
+    }
+    
+}
+
+Main();
---
+++ TASK5/Program.cs
@@ -0,0 +1,96 @@
+﻿//Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+/*Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+
+// код подходит для квадратных матриц с четными a b 
+void Main()
+            {
+                int a = 4;
+                int b = 4;
+                int[,] ourArray = new int[a, b];
+                FillArray(ourArray);
+                PrintArray(ourArray);
+            }
+
+            void FillArray(int[,] arr)
+            {
+                int i = 0;
+                int j;
+                int value = 1;
+
+                for (int count = 0; count < (arr.GetLength(0) + arr.GetLength(1)) / 4; count++)
+                
+               {
+
+
+                    for (j = count; j < arr.GetLength(1) - count; j++)//stop = 4-1=3 
+                    {
+                        arr[i, j] = value;
+                        value++;
+                        
+                       
+                    }
+                    j--;
+                  
+                    for (i++; i <= arr.GetLength(0) - count - 1; i++)//stop = 3 i = 1 j= 3 // c=1 stop = 2 i = 2 j = ?(2)
+                    {
+                        arr[i, j] = value;
+                        value++;
+                       
+                    }
+                    i--;
+                    for (j--; j >= count; j--)//stop =0 i = 3 j=2 //stop = 1 i=2 j=1
+                    {
+                        arr[i, j] = value;
+                        value++;
+                      
+                    }
+                    j++;
+                    
+                    for (i--; i >= count + 1; i--)// stop = 1 i =2 j= 0 
+                    {
+                       
+                        
+                        
+                            arr[i, j] = value;
+                            value++;
+                        
+                    }
+                    i++;
+
+
+
+
+
+                }
+            }
+
+
+
+            void PrintArray(int[,] arr)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] < 10)
+                        {
+                            Console.Write($" 0{arr[i, j]}");
+
+                        }
+                        else
+                        {
+                            Console.Write($" {arr[i, j]}");
+                        }
+                    }
+                    Console.WriteLine();
+
+                }
+
+
+            }
+            Main();
